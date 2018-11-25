99b2660f3cedbede8636bd8bdf3f1d184644b287
SaadMansourAmeur <saadm.ameur@gmail.com>
2018-11-25 16:15:45 +0100

    Question3 - Update user

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,7 @@ use AppBundle\Entity\User;
 use AppBundle\Entity\Image;
 use \Datetime;
 use AppBundle\Controller\Action;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
@@ -78,7 +79,7 @@ class DefaultController extends Controller
 
 
      /**
-     * @Route("/all", name="AllUsers")
+     * @Route("/all", name="allUsers")
      */
     public function getAllUserAction()
     {
@@ -88,6 +89,87 @@ class DefaultController extends Controller
                 $user->setImagePath($image->getPath());
           }
 
-          return $this->render('default/AllUser.html.twig', array('users' =>   $users));
+          return $this->render('default/allUser.html.twig', array('users' =>   $users));
     }
+
+
+
+    /**
+     * @Route("/update", name="updateUser")
+     */
+    public function updateUserAction(Request $request)
+    {
+        $formUpdate = $this->createForm('AppBundle\Form\UserType',null,array(
+                    // To set the action use $this->generateUrl('route_identifier')
+                    'action' => $this->generateUrl('updateUser'),
+                    'method' => 'POST'
+        )); 
+
+        $session = new Session();
+        
+        if ($request->isMethod('POST')) {
+
+            // Step 1  get user by id
+            if ($request->request->get('userId') != null) {
+                $user = $this->getDoctrine()->getRepository(User::class)->find($request->request->get('userId'));
+
+            
+                $image = $this->getDoctrine()->getRepository(Image::class)->find($user->getImageId());
+                $user->setImagePath($image->getPath());
+
+                $formUpdate = $this->createForm('AppBundle\Form\UserType',$user,array(
+                            // To set the action use $this->generateUrl('route_identifier')
+                            'action' => $this->generateUrl('updateUser'),
+                            'method' => 'POST'
+                ));
+                //$session->start();
+                $session->set('userId', $request->request->get('userId'));
+                $session->set('imageId', $user->getImageId());
+            }
+
+            // Step 2 update user
+            $formUpdate->handleRequest($request);
+
+            if($formUpdate->isValid()){
+                $user = new User();
+                $image = new Image();
+                $data = $formUpdate->getData();
+
+
+                // Get connections
+                $entityManager = $this->getDoctrine()->getManager();
+
+
+                // Write your raw SQL
+                $rawQuery1 = "update image SET path = '" . $data['imageId'] . "' where id = " . $session->get('imageId');
+
+                // Prepare the query from DATABASE1
+                $statementDB1 = $entityManager->getConnection()->prepare($rawQuery1);
+
+                // Execute both queries
+                $statementDB1->execute();
+
+                $rawQuery2 = "update user 
+                                SET gender = " . $data['gender'] . ", 
+
+                                firstname = '" . $data['firstname'] . "',
+
+                                lastname = '" . $data['lastname'] . "',
+
+                                birthDate = '" . $data['birthDate']->format('Y-m-d H:i:s') . "'
+
+                                where id = " . $session->get('userId');
+
+                $statementDB2 = $entityManager->getConnection()->prepare($rawQuery2);
+                
+                $statementDB2->execute();
+
+                return $this->redirectToRoute('allUsers');
+            }
+               
+        }
+
+         return $this->render('default/updateUser.html.twig', array('user' => $user, 'form' => $formUpdate->createView()));
+    }
+
 }
